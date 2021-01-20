39d6a741ee295889a261e7d96187c02c77dd7016
BoopathyRajaElangovan <boopathyye@gmail.com>
2021-01-20 15:50:59 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(function(req, res, next) {
